ff0866c72e709bd8f56dc9bc37bef7b791f8f6e2
Jejedm <146142644+Jejedm@users.noreply.github.com>
2023-11-17 05:44:31 -0500

    Phase 1 complete

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+/*
+    Javascript Basics tutorial
+*/ 
+
+let textValue = "Hello World";
+const headingElement = document.querySelector('h1');
+headingElement.textContent = textValue;
+
+const imageElement = document.querySelector('img');
+//imageElement.style.display = "none";
+
+imageElement.classList.add('gone');
